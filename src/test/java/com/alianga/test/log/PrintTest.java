a86872b59124bf8e17e49c4d326390f8f45038c8
zhengmingliang <zml@alianga.com>
2026-09-13 18:51:44 +0800

    test(log): 添加打印测试示例代码
    - 新增 PrintTest 类的 main 方法用于测试打印功能
    - 包含 normal、warning 和 error 三种打印方法调用示例
    - 添加了本地和外部 Print 类的打印调用对比
    - 注释了设置默认语言环境的代码行

--- src/test/java/com/alianga/test/log/PrintTest.java
+++ src/test/java/com/alianga/test/log/PrintTest.java
@@ -1,4 +1,19 @@
 package com.alianga.test.log;
 
+import com.aayushatharva.brotli4j.common.annotations.Local;
+import com.alianga.jkit.Print;
+
+import java.util.Locale;
+
 public class PrintTest {
+    public static void main(String[] args) {
+//        Locale.setDefault(Locale.ENGLISH);
+        Print.normal("123");
+        Print.warning("123");
+        Print.error("123");
+        System.out.println("-------");
+        top.wys.utils.Print.normal("123");
+        top.wys.utils.Print.warning("123");
+        top.wys.utils.Print.error("123");
+    }
 }
